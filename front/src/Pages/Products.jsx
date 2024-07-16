3c7285f92f46370e536943bd8fb376f5169e780b
Vardidis <44882528+Vardidis@users.noreply.github.com>
2024-07-17 01:49:20 +0300

    products edit mobile, desktop

--- front/src/Pages/Products.jsx
+++ front/src/Pages/Products.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import bg1 from '../assets/giotis-wheelie.jpg';
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import WheelchairPickupIcon from '@mui/icons-material/WheelchairPickup';
 import AlbumIcon from '@mui/icons-material/Album';
 import SupportIcon from '@mui/icons-material/Support';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
-import ViewQuiltIcon from '@mui/icons-material/ViewQuilt';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import Chip from '@mui/material/Chip';
+import { Context } from "../Components/Context";
 
 const Products = () => {
+    const {isDesktop} = useContext(Context);
     const [view, setView] = useState('list');
 
     const handleChange = (event, nextView) => {
@@ -22,60 +22,115 @@ const Products = () => {
         console.info('You clicked the delete icon.');
     };
 
-    return(              
-        <Stack spacing={2} sx={{ padding: 5 }}>     
-            <Stack direction='row' spacing={8}>
-                <Stack spacing={1}>
-                    <div className='product-menu'>
-                        Όλα τα προϊόντα
-                    </div>
-                    <div className='product-menu'>
-                        <WheelchairPickupIcon fontSize="small"/>
-                        Αμαξίδια
-                    </div>
-                    <div className='product-menu'>
-                        <AlbumIcon fontSize="small"/>
-                        Εξαρτήματα
-                    </div>
-                    <div className='product-menu'>
-                        <SupportIcon fontSize="small"/>
-                        Βοηθήματα
-                    </div>
+    if(isDesktop){
+        return(              
+            <Stack spacing={4} sx={{ paddingLeft: {lg: 10, md: 5}, paddingRight: {lg: 10, md: 5}, width: {lg: 715, md: 700}, marginTop: 3 }}>     
+                <Stack direction='row' sx={{ alignItems: 'center', justifyContent: 'space-between' }}>   
+                    <Typography fontSize={26} fontWeight={600}>Κατάστημα</Typography>           
+                    <ToggleButtonGroup
+                        size="small"
+                        value={view}
+                        exclusive
+                        onChange={handleChange}
+                    >
+                        <ToggleButton value="list" aria-label="list">
+                            <ViewListIcon />
+                        </ToggleButton>
+                        <ToggleButton value="module" aria-label="module">
+                            <ViewModuleIcon />
+                        </ToggleButton>
+                    </ToggleButtonGroup>
                 </Stack>
-                <Stack spacing={2}>
-                <Stack direction='row' spacing={1}>
-                <Chip label="R9" onDelete={handleDelete} />
-                <Chip label="S7" onDelete={handleDelete} />
-                <ToggleButtonGroup
-                    size="small"
-                    value={view}
-                    exclusive
-                    onChange={handleChange}
-                >
-                    <ToggleButton value="list" aria-label="list">
-                        <ViewListIcon />
-                    </ToggleButton>
-                    <ToggleButton value="module" aria-label="module">
-                        <ViewModuleIcon />
-                    </ToggleButton>
-                    <ToggleButton value="quilt" aria-label="quilt">
-                        <ViewQuiltIcon />
-                    </ToggleButton>
-                </ToggleButtonGroup>
+                <Stack direction='row' spacing={{lg: 6, md: 4}}>
+                    <Stack spacing={1}>
+                        <div className='product-menu'>
+                            Όλα τα προϊόντα
+                        </div>
+                        <div className='product-menu'>
+                            <WheelchairPickupIcon fontSize="small"/>
+                            Αμαξίδια
+                        </div>
+                        <div className='product-menu'>
+                            <AlbumIcon fontSize="small"/>
+                            Εξαρτήματα
+                        </div>
+                        <div className='product-menu'>
+                            <SupportIcon fontSize="small"/>
+                            Βοηθήματα
+                        </div>
+                    </Stack>
+                    <Stack spacing={2}>                
+                        <Stack spacing={2} direction='row'>
+                            <img className='product-img' src={bg1}/> 
+                            <img className='product-img' src={bg1}/> 
+                        </Stack>    
+                        <Stack spacing={2} direction='row'>
+                            <img className='product-img' src={bg1}/> 
+                            <img className='product-img' src={bg1}/> 
+                        </Stack>    
+                        <Stack spacing={2} direction='row'>
+                            <img className='product-img' src={bg1}/> 
+                            <img className='product-img' src={bg1}/> 
+                        </Stack>    
+                    </Stack>                                                                  
+                </Stack>            
             </Stack>
-                    <Stack spacing={2} direction='row'>
-                        <img className='product-img' src={bg1}/> 
-                        <img className='product-img' src={bg1}/> 
-                    </Stack>    
-                    <Stack spacing={2} direction='row'>
-                        <img className='product-img' src={bg1}/> 
-                        <img className='product-img' src={bg1}/> 
-                    </Stack>    
-                </Stack>                
-                                                  
-            </Stack>            
-        </Stack>
-    );
+        );
+    }else{
+        return(
+            <Stack spacing={4} sx={{ paddingLeft: {sm: 7, xs: 2}, paddingRight: {sm: 7, xs: 2}, width: {sm: 625, xs: 600}, marginTop: 3 }}>     
+                <Stack direction='row' sx={{ alignItems: 'center', justifyContent: 'space-between' }}>   
+                    <Typography fontSize={{sm: 26, xs: 22}} fontWeight={600}>Κατάστημα</Typography>           
+                    <ToggleButtonGroup
+                        size="small"
+                        value={view}
+                        exclusive
+                        onChange={handleChange}
+                    >
+                        <ToggleButton value="list" aria-label="list">
+                            <ViewListIcon />
+                        </ToggleButton>
+                        <ToggleButton value="module" aria-label="module">
+                            <ViewModuleIcon />
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </Stack>
+                <Stack spacing={{sm: 3}}>
+                    <Stack direction='row' spacing={1}>
+                        <div className='product-menu'>
+                            Όλα τα προϊόντα
+                        </div>
+                        <div className='product-menu'>
+                            <WheelchairPickupIcon fontSize="small"/>
+                            Αμαξίδια
+                        </div>
+                        <div className='product-menu'>
+                            <AlbumIcon fontSize="small"/>
+                            Εξαρτήματα
+                        </div>
+                        <div className='product-menu'>
+                            <SupportIcon fontSize="small"/>
+                            Βοηθήματα
+                        </div>
+                    </Stack>
+                    <Stack spacing={1}>                
+                        <Stack spacing={1} direction='row'>
+                            <img className='product-img-mob' src={bg1}/> 
+                            <img className='product-img-mob' src={bg1}/> 
+                        </Stack>    
+                        <Stack spacing={1} direction='row'>
+                            <img className='product-img-mob' src={bg1}/> 
+                            <img className='product-img-mob' src={bg1}/> 
+                        </Stack>    
+                        <Stack spacing={1} direction='row'>
+                            <img className='product-img-mob' src={bg1}/> 
+                            <img className='product-img-mob' src={bg1}/> 
+                        </Stack>  
+                    </Stack>                                                                  
+                </Stack>            
+            </Stack>
+        );
+    }
 }
 
 export default Products;
