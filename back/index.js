b6adf62146bd7f6b16f497ac0a8837ac53304bee
Vardidis <fivos.vardis@gmail.com>
2024-09-15 15:50:31 +0300

    minor

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['https://gs-wheels-server.vercel.app/'],
+        origin: ['https://gs-wheels-ghcdl706z-vardidis-projects.vercel.app'],
         methods: ['POST', 'GET']
     }
 ));
