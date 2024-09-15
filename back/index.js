da7604c33432f98d492904e46c74a0f4038f7263
Vardidis <fivos.vardis@gmail.com>
2024-09-15 16:14:53 +0300

    test env vars

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['https://gs-wheels-vardidis-projects.vercel.app'],
+        origin: ['https://gs-wheels-vardidis-projects.vercel.app', 'https://gs-wheels-1w2d1z471-vardidis-projects.vercel.app'],
         methods: ['POST', 'GET']
     }
 ));
