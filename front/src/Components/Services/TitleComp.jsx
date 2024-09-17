4eac177ecb463453d92ad17182d03ff7662a44be
Vardidis <fivos.vardis@gmail.com>
2024-09-17 18:32:06 +0300

    change funcTabs

--- front/src/Components/Services/TitleComp.jsx
+++ front/src/Components/Services/TitleComp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Typography } from '@mui/material';
 const TitleComp = (props) => {
     return(
-        <Box sx={{ paddingTop: 3, paddingBottom: {xl: 10, lg: 10, md: 8, sm: 8, xs: 6, xxs: 4} }}>            
+        <Box sx={{ paddingTop: 3, paddingBottom: {xl: 8, lg: 6, md: 6, sm: 6, xs: 4, xxs: 3} }}>            
             <Typography fontSize={{xl: 28, lg: 24, md: 24, sm: 24, xs: 22, xxs: 18}} fontWeight={600} sx={{ textAlign: 'center' }}>
                 {props.text}
             </Typography>     
