5b224261f1add0adb25fb49a8ad58d79f79268aa
Vardidis <fivos.vardis@gmail.com>
2024-09-21 00:25:58 +0300

    changes in service texts

--- front/src/Components/Services/TextWindow.jsx
+++ front/src/Components/Services/TextWindow.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import AccordionComp from "../AccordionComp";
 
-const TextWindow = () => {
+const TextWindow = (props) => {
     return(
-        <>
-        </>
+        <Stack spacing={2}>
+            <Typography variant='h7' sx={{ fontWeight: 'bold', color: 'rgb(40, 40, 40)' }}>
+                {props.title}
+            </Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'center', bgcolor: 'rgb(255, 252, 252)', padding: 3, borderRadius: 2, border: '1px solid rgb(180, 180, 180)' }}>
+                <Typography sx={{ color: 'rgb(40, 40, 40)' }}>
+                    {props.text}
+                </Typography>          
+            </Box>
+        </Stack>
     );
 }
 
