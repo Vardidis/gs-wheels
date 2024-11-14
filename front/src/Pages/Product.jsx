1430d97ba2b8e973ca3e215a20953e9144a56dd7
Vardidis <fivos.vardis@gmail.com>
2024-11-14 14:49:51 +0200

    fix product view

--- front/src/Pages/Product.jsx
+++ front/src/Pages/Product.jsx
@@ -23,7 +23,8 @@ const DetailsBox = (props) => {
 const Product = () => {
     const {productId} = useParams();
     const {allProducts, isDesktop} = useContext(Context);
-    const productItem = allProducts.filter((product) => product.id === String(productId))[0];
+    const productItem = allProducts.filter((prod) => {return prod._id === String(productId)})[0] || null;
+    console.log(productItem)
 
     const style = isDesktop ? {
         display: 'flex',
