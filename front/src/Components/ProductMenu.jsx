156d1dddb8815516e9f7a070e7bca494956dd5aa
Vardidis <fivos.vardis@gmail.com>
2024-12-20 14:25:57 +0200

    eshop shange

--- front/src/Components/ProductMenu.jsx
+++ front/src/Components/ProductMenu.jsx
@@ -7,7 +7,7 @@ const ProductMenu = (props) => {
         <Grid container columnGap={2} rowGap={2} justifyContent={'center'} sx={{ padding: '32px 0px' }}>
             {props.items.map((item, index) => {                                
                 return(
-                    <Grid item key={item._id} xs={12} sm={5} sx={{ marginBottom: 3 }}>                            
+                    <Grid item key={item._id} xs={12} sm={2.5} sx={{ marginBottom: 3 }}>                            
                         <ProductItem item={item} index={item._id} key={index}/>                             
                     </Grid>
                 );
